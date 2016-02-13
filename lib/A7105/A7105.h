7b6eb3e81270f5f112ca78a121d5dedf3d02a40c
Liam Marshall <liam@cpan.org>
2016-02-13 10:01:45 -0600

    Organize the headers a bit

--- lib/A7105/A7105.h
+++ lib/A7105/A7105.h
@@ -19,19 +19,18 @@ public:
   A7105(u8 _cs_pin);
 
   void begin();
-  
-  void write_register(u8 reg, u8 data);
-  u8 read_register(u8 reg);
+  void reset();
 
   void dump_registers();
-
-  void reset();
 
 private:
   inline void t_start() {digitalWrite(cs_pin, LOW);}
   inline void t_end() {digitalWrite(cs_pin, HIGH);}
 
   void init_registers();
+
+  void write_register(u8 reg, u8 data);
+  u8 read_register(u8 reg);
 
   u8 cs_pin;
 };
